42ac2dc505c6cdb9b4742ac0123d357023cc37bc
skyglass <skyglass2001@gmail.com>
2023-07-08 18:08:51 +0200

    added gateway

--- src/main/java/com/ivanfranchin/moviesapi/config/SwaggerConfig.java
+++ src/main/java/com/ivanfranchin/moviesapi/config/SwaggerConfig.java
@@ -1,11 +1,9 @@
 package com.ivanfranchin.moviesapi.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.OAuthFlow;
-import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,19 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@io.swagger.v3.oas.annotations.security.SecurityScheme(
-  name = "security_auth", type = SecuritySchemeType.OAUTH2,
-  flows = @OAuthFlows(
-    authorizationCode = @OAuthFlow(
-      authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
-      tokenUrl = "${springdoc.oAuthFlow.tokenUrl}"
-      // scopes = {
-      //   @OAuthScope(name = "product:read", description = "read scope"),
-      //   @OAuthScope(name = "product:write", description = "write scope")
-      // }
-    )
-  )
-)
 public class SwaggerConfig {
 
     @Value("${spring.application.name}")
@@ -35,20 +20,37 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(
-                        new Components().addSecuritySchemes(BEARER_KEY_SECURITY_SCHEME,
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
-                .info(new Info().title(applicationName));
+                        new Components()
+                            .addSecuritySchemes(BEARER_KEY_SECURITY_SCHEME,
+                                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
+                )
+                .info(new Info().title(applicationName))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_KEY_SECURITY_SCHEME));
     }
 
     @Bean
     public GroupedOpenApi customApi() {
-        return GroupedOpenApi.builder().group("movie").pathsToMatch("/movies/api/**").build();
+        return GroupedOpenApi.builder()
+                .group("movies")
+                /*.addOpenApiCustomiser(openApi -> {
+                    openApi.getServers().clear();
+                    openApi.addServersItem(new Server().url("https://books.greeta.net"));
+                })*/
+                .pathsToMatch("/movies/**")
+                .build();
     }
 
     @Bean
     public GroupedOpenApi actuatorApi() {
-        return GroupedOpenApi.builder().group("actuator").pathsToMatch("/actuator/**").build();
+        return GroupedOpenApi.builder()
+                .group("actuator")
+                /*.addOpenApiCustomiser(openApi -> {
+                    openApi.getServers().clear();
+                    openApi.addServersItem(new Server().url("https://books.greeta.net"));
+                })*/
+                .pathsToMatch("/actuator/**").build();
     }
 
     public static final String BEARER_KEY_SECURITY_SCHEME = "bearer-key";
+
 }
--- src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
+++ src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
@@ -24,29 +28,38 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/movies", "/movies/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/actuator/**", "/movies/actuator/**").permitAll()
+        return http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/", "/movies/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .requestMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
                 .requestMatchers("/movies/*/comments").hasAnyRole(MOVIES_MANAGER, USER)
-                .requestMatchers("/movies", "/movies/**").hasRole(MOVIES_MANAGER)
+                .requestMatchers("/", "/movies/**").hasRole(MOVIES_MANAGER)
                 .requestMatchers("/userextras/me", "/movies/userextras/me").hasAnyRole(MOVIES_MANAGER, USER)
-                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**",
-								"/openapi/**", "/webjars/**", "/oauth2/**", "/login/**", "/error/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthConverter);
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().and().csrf().disable();
-        return http.build();
+                .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(AbstractHttpConfigurer::disable)
+        .build();
     }
+
 
     @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+    /*@Bean
     public JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).jwsAlgorithm(SignatureAlgorithm.RS256).build();
-    }
+    }*/
 
     public static final String MOVIES_MANAGER = "MOVIES_MANAGER";
     public static final String USER = "USER";
